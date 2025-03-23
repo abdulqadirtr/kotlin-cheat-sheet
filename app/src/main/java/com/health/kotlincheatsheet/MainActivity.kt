0cffb7fae30041fe1a4118241146284be549de3a
abdulqadirtr <qadirabdul20@yahoo.com>
2025-03-23 16:57:08 +0100

    * Lazy colum added. * Search Bar Implemented.

--- app/src/main/java/com/health/kotlincheatsheet/MainActivity.kt
+++ app/src/main/java/com/health/kotlincheatsheet/MainActivity.kt
@@ -3,13 +3,38 @@ package com.health.kotlincheatsheet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.health.kotlincheatsheet.ui.theme.KotlinCheatSheetTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +47,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    DefaultPreview()
                 }
             }
         }
@@ -34,10 +59,118 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun SearchBar(modifier: Modifier = Modifier){
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(72.dp) // Set a fixed height for the search bar
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null
+                )
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                unfocusedIndicatorColor = Color.Transparent, // No indicator color when unfocused
+                focusedIndicatorColor = Color.Transparent// Background color of the TextField
+            ),
+            placeholder = {
+                Text(stringResource(R.string.placeholder_search))
+            },
+            modifier = modifier
+                .fillMaxWidth()
+                .heightIn(min = 56.dp)
+        )
+    }
+}
+
+@Composable
+fun AllignYourBody(modifier: Modifier){
+    Column (){
+        Image(painterResource(R.drawable.ic_launcher_background), contentDescription = "Content")
+        Text("Inversion")
+    }
+}
+
+@Composable
+fun AllignYourBodyContent(@DrawableRes drawable : Int,
+                          @StringRes text : Int,
+                          modifier : Modifier = Modifier
+                          ){
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(text),
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
+            style = MaterialTheme.typography.body1,
+        )
+    }
+
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun AlignYourBodyRowPreview() {
+  //  KotlinCheatSheetTheme { AlignYourBodyRow() }
+}
+
+@Composable
+fun AlignYourBodyRow(modifier: Modifier = Modifier){
+
+    LazyRow (
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ){
+        items(alignYourBodyData){
+            item -> AllignYourBodyContent(item.drawable, item.text)
+        }
+
+    }
+
+
+}
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
+
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+).map { DrawableStringPair(it.first, it.second) }
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     KotlinCheatSheetTheme {
-        Greeting("Android")
+        Column(modifier = Modifier.fillMaxSize()) {
+            SearchBar(Modifier.padding(8.dp))
+            AlignYourBodyRow()
+        }
+
+       // AllignYourBody(modifier = Modifier)
     }
 }
